b55fb3f65ecfe3a50b08eba323a9f8f7a8285050
Sch <junhg0211@gmail.com>
2025-06-04 05:34:00 +0000

    lomg

--- libs/abeliqua.py
+++ libs/abeliqua.py
@@ -40,7 +40,7 @@ def days_to_abeliqua(days) -> AbeliquaTime:
         year += 1
 
     month = 1
-    while count > le := (get_month_length(month, year)):
+    while count > (le := get_month_length(month, year)):
         count -= le
         month += 1
 
