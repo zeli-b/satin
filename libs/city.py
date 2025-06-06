5abd8ff8e2247f1216dd0c95e7e3310ac8716534
Sch <junhg0211@gmail.com>
2025-06-06 08:00:52 +0000

    city lib

---
+++ libs/city.py
@@ -0,0 +1,50 @@
+from json import dump, load
+
+try:
+    with open(FILENAME, "r") as file:
+        data = load(file)
+    cities = data
+except:
+    cities = dict()
+
+
+def create_city(name, owner):
+    cities[name] = [owner, 0, 0]
+    save()
+
+
+def get_owner_account(name):
+    if name not in cities:
+        return
+    return cities.get(name)[0]
+
+
+def get_area(name):
+    if name not in cities:
+        return
+    return cities.get(name)[1]
+
+
+def get_population(name):
+    if name not in cities:
+        return
+    return cities.get(name)[2]
+
+
+def set_area(name, area):
+    if name not in cities:
+        return
+    cities[name][1] = area
+    save()
+
+
+def set_population(name, population):
+    if name not in cities:
+        return
+    cities[name][2] = population
+    save()
+
+
+def save():
+    with open(FILENAME, "w") as file:
+        dump(data, file)
